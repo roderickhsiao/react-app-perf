6fa2780bb5ffde9ae96128d190a7adafe6a28ca3
Roderick Hsiao <roderickhsiao@gmail.com>
2017-06-10 17:14:11 -0700

    Add new file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends PureComponent {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
