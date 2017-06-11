f02445149d3ec1edb12e7ad18675539e4fa0b04c
Roderick Hsiao <roderickhsiao@gmail.com>
2017-06-10 17:14:18 -0700

    Push new component

---
+++ src/components/Grid.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Grid extends PureComponent {
+  render() {
+    return (
+      <div className="Grid"></div>
+    );
+  }
+}
+
+export default Grid;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Nav extends PureComponent {
+  render() {
+    return (
+      <div className="Nav"></div>
+    );
+  }
+}
+
+export default Nav;
