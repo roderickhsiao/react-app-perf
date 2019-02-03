67776ac7c2afa06e30f075a3893adbf46f1df871
Roderick Hsiao <roderickhsiao@gmail.com>
2019-02-02 16:04:45 -0800

    Push new file

--- src/page/MultipleInlineVideo.jsx
+++ src/page/MultipleInlineVideo.jsx
@@ -1,6 +1,28 @@
 import React, { PureComponent, Fragment } from "react";
 import PageCanvas from "../components/PageCanvas";
 
+class AutoPlayVideo extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      canPlay: false
+    };
+  }
+  render() {
+    return (
+      <video
+        className="Mx(12px) My(8px)"
+        onCanPlay={() => this.setState({ canPlay: true })}
+        autoPlay
+        loop
+        playsInline
+        muted
+        style={{ borderColor: this.state.canPlay ? "#00897b" : "transparent", borderWidth: '5px', borderStyle: 'solid' }}
+        src={this.props.src}
+      />
+    );
+  }
+}
 class MultipleInlineVideo extends PureComponent {
   render() {
     return (
@@ -10,12 +32,8 @@ class MultipleInlineVideo extends PureComponent {
           <Fragment>
             {Array(300)
               .fill(null)
-              .map(() => (
-                <video
-                  playInline
-                  muted
-                  src="https://media1.giphy.com/media/5heUAvp8TZwuT2mtgD/200w.mp4"
-                />
+              .map((item, index) => (
+                <AutoPlayVideo src="https://media1.giphy.com/media/5heUAvp8TZwuT2mtgD/200w.mp4" key={index} />
               ))}
           </Fragment>
         }
